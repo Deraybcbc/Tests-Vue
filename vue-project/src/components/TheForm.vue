71df1407c59665e4b292ae4147f40b92970887fc
Deraybcbc <a22kevburcac@inspedralbes.cat>
2024-01-18 11:40:01 +0100

    Test

---
+++ vue-project/src/components/TheForm.vue
@@ -0,0 +1,18 @@
+<template>
+    <form @submit.prevent="Submit">
+        <input v-model="email" type="email" placeholder="your@email.com" required />
+        <button type="submit">Submit</button>
+    </form>
+</template>
+
+<script>
+// ¡Recuerda! usar script lang="ts"
+import { ref } from 'vue'
+
+const email = ref('')
+
+const Submit = () => {
+    console.log(email.value)
+}
+
+</script>
